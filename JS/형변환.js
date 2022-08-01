6e4675957f245b035727fca436b10e63ef54a39e
서동경 <ehhdrud@icloud.com>
2022-08-01 23:25:26 +0900

    형변환

---
+++ JS/형변환.js
@@ -0,0 +1,62 @@
+//String
+console.log(String(123));
+console.log(String(1 / 0));
+console.log(String(-1 / 0));
+console.log(String(NaN));
+console.log(String(true));
+console.log(String(false));
+console.log(String(undefined));
+console.log(String(null));
+console.log(String("haha"));
+console.log(typeof String(123));
+console.log(typeof String(1 / 0));
+console.log(typeof String(-1 / 0));
+console.log(typeof String(NaN));
+console.log(typeof String(true));
+console.log(typeof String(false));
+console.log(typeof String(undefined));
+console.log(typeof String(null));
+console.log(typeof String("haha"));
+
+//Number
+console.log(Number(""));
+console.log(Number("123"));
+console.log(Number("hello"));
+console.log(Number("123hello"));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(undefined));
+console.log(Number(null)); //Number는 정수와 실수를 모두 포함하는 형변환
+console.log(typeof Number(""));
+console.log(typeof Number("123"));
+console.log(typeof Number("hello"));
+console.log(typeof Number("123hello"));
+console.log(typeof Number(true));
+console.log(typeof Number(false));
+console.log(typeof Number(undefined));
+console.log(typeof Number(null));
+console.log(parseInt("123.123")); //정수 변환
+console.log(parseFloat("123.123")); //실수 변환
+
+console.log(typeof parseInt("123.123"));
+console.log(typeof parseFloat("123.123"));
+
+//Boolean
+console.log(Boolean(""));
+console.log(Boolean("123"));
+console.log(Boolean("hello"));
+console.log(Boolean("0"));
+console.log(Boolean(0));
+console.log(Boolean(123));
+console.log(Boolean(NaN));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(typeof Boolean(""));
+console.log(typeof Boolean("123"));
+console.log(typeof Boolean("hello"));
+console.log(typeof Boolean("0"));
+console.log(typeof Boolean(0));
+console.log(typeof Boolean(123));
+console.log(typeof Boolean(NaN));
+console.log(typeof Boolean(null));
+console.log(typeof Boolean(undefined));
