fca6c29ab09951c46bd74fcf97ae285ea33ffa8f
서동경 <ehhdrud@icloud.com>
2022-08-02 20:39:18 +0900

    수정220802

--- JS/반복문(for..in).js
+++ JS/반복문(for..in).js
@@ -2,7 +2,7 @@ const person = { fname: "DongKyeong", lname: "Seo", age: 28 };
 
 let text = "";
 
-//for(key in object){}
+//Syntax: for(key in object){}
 for (let x in person) {
   text += person[x]; //text = text + person[x] (person객체의 각 key들이 for문을 통해 할당되고, 더해짐)
 }
--- JS/조건문(3항연산자).js
+++ JS/조건문(3항연산자).js
@@ -1,4 +1,4 @@
 let age = 20;
 
-msg = age < 19 ? "The user is not adult." : "The user is adult.";
+msg = age < 19 ? "The user is not adult." : "The user is adult."; //변수 = 조건식 ? 참일 때 값:거짓일 때 값
 console.log(msg);
