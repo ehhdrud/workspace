ddfa53f4a4fc01ac5858ce2ac830980a8a1a42ca
서동경 <ehhdrud@icloud.com>
2022-08-09 13:20:46 +0900

    프리티어적용

--- JS/06_원시타입boolean.js
+++ JS/06_원시타입boolean.js
@@ -1,6 +1,5 @@
 let name_check = true;
 let age_check = false;
 
-let sdk = 10>3;
+let sdk = 10 > 3;
 console.log(sdk);
-
--- JS/08_원시타입number.js
+++ JS/08_원시타입number.js
@@ -2,9 +2,7 @@ let num_1 = 123.0;
 let num_2 = 123.456;
 let num_3 = 1 / 0;
 
-
 console.log(num_1 - num_2);
 console.log((num_1 - num_2).toFixed(3)); //소수점 3번째 까지만 보여줌! 나머지는 반올림 처리
 console.log(num_3);
 console.log(num_1 / "hello"); //숫자가 아니므로 'Not A Number'(NaN)
-
