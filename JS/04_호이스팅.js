ae674dd610ea1da487ce3f2e6d84820194714718
서동경 <ehhdrud@icloud.com>
2022-08-10 21:57:12 +0900

    MOD-220809

--- JS/04_호이스팅.js
+++ JS/04_호이스팅.js
@@ -1,5 +1,5 @@
 console.log(name_1);
-var name_1 = "SDK";
+var name_1 = "SDK"; //선언은 전역으로 됐지만, 할당(초기화)은 2번 라인에서 됨
 console.log(name_1);
 
 //1,2,3번 라인 내부 동작원리
@@ -8,6 +8,5 @@ console.log(name_2);
 name_2 = "SDK";
 console.log(name_2);
 
-// ReferenceError: Cannot access 'name_3' before initialization //let을 쓰면 hoisting 안되므로 에러
 /*console.log(name_3);
-let name_3 = "SDK";*/
+let name_3 = "SDK"; // ReferenceError: Cannot access 'name_3' before initialization //let을 쓰면 hoisting 안되므로 에러 */
