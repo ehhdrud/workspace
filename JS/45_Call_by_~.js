7e4cb0d068866e0f532e1b61fb20c13d567b168a
서동경 <ehhdrud@icloud.com>
2022-08-15 22:19:42 +0900

    220815

--- JS/45_Call_by_~.js
+++ JS/45_Call_by_~.js
@@ -2,14 +2,14 @@
 let a = 1;
 let add = function (b) {
   b = b + 1;
-}; //callee
-add(a); //caller
-console.log(a); //output:1 -> b가 2가 되어도 a는 변하지 않음!
+};
+add(a);
+console.log(a);
 
 //Call by reference
 var a_ = { v: 1 };
 var add_ = function (b_) {
   b_.v = b_.v + 1;
-}; //callee
-add_(a_); //caller
-console.log(a_); //output:2 -> b_가 2가 되면 a_도 같이 변함!
+};
+add_(a_);
+console.log(a_);
