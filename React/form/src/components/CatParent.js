bd7c8383306cc25d066e38394042ac50c0eb4c9e
서동경 <ehhdrud@icloud.com>
2023-04-18 22:34:37 +0900

    230418

--- React/form/src/components/CatParent.js
+++ React/form/src/components/CatParent.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Cat from "./Cat";
 
 export default function CatParent() {
@@ -7,17 +7,38 @@ export default function CatParent() {
   console.log("부모 컴포넌트 CatParent");
   console.log(catRef);
 
+  const ageRef = useRef(1);
+  const [state, setState] = useState(1);
+
+  console.log(ageRef.current);
+
   return (
     <div>
       <h4> 고양이가 세상을 구한다 ️</h4>
       <div>
         <Cat a="a" ref={catRef} />
+        <h4>나이: {ageRef.current}</h4>
+        <h4>상태값: {state}</h4>
         <button
           onClick={() => {
             alert(catRef.current.height);
           }}
         >
           고양이의 크기를 알고싶어
+        </button>
+        <button
+          onClick={() => {
+            ageRef.current = ageRef.current + 1;
+          }}
+        >
+          새해
+        </button>
+        <button
+          onClick={() => {
+            setState(state + 1);
+          }}
+        >
+          리렌더링
         </button>
       </div>
     </div>
