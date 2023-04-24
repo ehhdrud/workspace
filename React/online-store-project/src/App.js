147801ec5d990ae3427b75251c144bc9db152bc1
서동경 <ehhdrud@icloud.com>
2023-04-24 16:42:40 +0900

    230424

--- React/online-store-project/src/App.js
+++ React/online-store-project/src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
 import OnlineStore from "./components/OnlineStore";
 
 export default function App() {
   return (
-    <div>
+    <>
       <OnlineStore />
-    </div>
+    </>
   );
 }
--- React/online-store-project/src/components/ProductsTable.js
+++ React/online-store-project/src/components/ProductsTable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as S from "./ProductsTable.style";
 
 export default function ProductsTable(props) {
   const { category, items, inStockOnly } = props;
@@ -9,14 +10,14 @@ export default function ProductsTable(props) {
   return (
     <table>
       <tr>
-        <td style={{ fontWeight: "bold" }}>{category}</td>
+        {/* <td style={{ fontWeight: "bold" }}>{category}</td> */}
+        <S.Category>{category}</S.Category>
       </tr>
 
       {filteredItems.map((item, idx) => (
         <tr key={idx}>
-          <td style={item.stocked ? { color: "black" } : { color: "red" }}>
-            {item.name}
-          </td>
+          {/* <td style={item.stocked ? { color: "black" } : { color: "red" }}>{item.name}</td> */}
+          <S.ProductName stocked={item.stocked}>{item.name}</S.ProductName>
           <td>{item.price}</td>
         </tr>
       ))}
