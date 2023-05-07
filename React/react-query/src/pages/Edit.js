9ae66bca8cf4a392d58b26f846ef4a30296742ae
서동경 <ehhdrud@icloud.com>
2023-05-08 02:59:44 +0900

    230508

---
+++ React/react-query/src/pages/Edit.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { getUser, updateNickname } from "../mocks/api";
+
+export default function Edit() {
+  const [inputValue, setInputValue] = useState("");
+  const queryClient = useQueryClient();
+  const { data } = useQuery("@getUser", getUser);
+
+  const mutation = useMutation(updateNickname, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("@getUser");
+    },
+  });
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutation.mutate(inputValue);
+  };
+
+  return (
+    <>
+      <h1>Edit</h1>
+      {data && <h3>현재 닉네임: {data.nickName}</h3>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          변경할 닉네임:
+          <input type="text" value={inputValue} onChange={handleChange} />
+        </label>
+      </form>
+    </>
+  );
+}
