81891f8e53baba1565dda13c24554161bddde465
서동경 <ehhdrud@icloud.com>
2023-07-01 15:13:44 +0900

    230701

--- React/context/src/App.js
+++ React/context/src/App.js
@@ -12,7 +12,6 @@ function App() {
   // root url : main page component
   // tech url : tech page component
   // blog url : blog page component
-
   return (
     <UserStore>
       <BrowserRouter>
@@ -23,8 +22,6 @@ function App() {
               <Route path="javascript" element={<JavascriptPage />} />
               <Route path="react" element={<ReactPage />}>
                 {/* 세미콜론(:)은 이 경로 매개변수를 정의하는 문법으로, 이를 통해 URL 경로에 동적인 값을 전달할 수 있다. */}
-                {/* 중첩된 라우트가 있는 페이지는 부모 경로의 컴포넌트와 결합되어 하나의 페이지에 렌더링된다. */}
-                {/* 이를 원하지 않는다면 중첩 라우트를 사용하지 않고, 형제 컴포넌트로 작성하면 된다. */}
                 {/* 이 때 path는 "react/:docId"로 수정해주어야 한다. */}
                 <Route path=":docId" element={<ReactDocPage />} />
               </Route>
