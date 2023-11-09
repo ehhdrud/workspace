ee21561a2c5e1370636e1b3ed3bd345a2bd6b2cf
서동경 <ehhdrud@icloud.com>
2023-11-09 15:44:02 +0900

    231109 - '클래스 맴버는 enumerable false' 증명

---
+++ JavaScript/클래스/클래스맴버는 enumerable false.js
@@ -0,0 +1,23 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getInfo() {
+        console.log(`${this.name}은 ${this.age}살입니다`);
+    }
+}
+
+const myInfo = new Person('서동경', 29);
+
+myInfo.getInfo();
+
+for (const prop in myInfo) {
+    console.log(prop);
+}
+
+for (const method in myInfo) {
+    if (typeof myInfo[method] === 'function') {
+        console.log(method);
+    }
+}
