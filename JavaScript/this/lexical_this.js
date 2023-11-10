c35d37ec37436a8d1d6290a02bf6648ab516af51
서동경 <ehhdrud@icloud.com>
2023-11-11 01:15:27 +0900

    lexical this 추가 증명

--- JavaScript/this/lexical_this.js
+++ JavaScript/this/lexical_this.js
@@ -18,3 +18,16 @@ function 화살표함수사용() {
 
 const 일반함수인스턴스 = new 일반함수사용();
 const 화살표함수인스턴스 = new 화살표함수사용();
+
+function Person(name) {
+    this.name = name;
+
+    // 화살표 함수의 this는 상위 스코프인 Person 함수의 this와 동일하다.
+    // 따라서 이 함수를 사용하여 인스턴스를 생성하면, 실제로는 전역 객체를 참조하게 된다.
+    this.getName = () => {
+        console.log(this.name);
+    };
+}
+
+const person = new Person('서동경');
+person.getName();
